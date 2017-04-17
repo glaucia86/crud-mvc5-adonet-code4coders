3112c3a96f095e071cf16e2a08adec3868303c26
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-04-16 21:25:21 -0300

    Update dbo.DeletarFuncionario.sql

--- dbo.DeletarFuncionario.sql
+++ dbo.DeletarFuncionario.sql
@@ -1,6 +1,7 @@
-﻿CREATE PROCEDURE [dbo].[Procedure]
-	@param1 int = 0,
-	@param2 int
-AS
-	SELECT @param1, @param2
-RETURN 0
+CREATE PROCEDURE [dbo].[DeletarFuncionarioPorId]
+(
+	@FuncionarioId int
+)
+AS BEGIN
+	delete from Funcionario	where Id = @FuncionarioId
+END
