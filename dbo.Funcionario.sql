d1b7508cd2ae3e5386132a87c99b6ecdb4ac8ee7
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-04-17 23:21:53 -0300

    Adicionando a table 'Funcionario.'

---
+++ dbo.Funcionario.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Funcionario]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Nome] NVARCHAR(50) NULL, 
+    [Sobrenome] NVARCHAR(50) NULL, 
+    [Cidade] NVARCHAR(50) NULL, 
+    [Endereco] NVARCHAR(50) NULL, 
+    [Email] NVARCHAR(30) NULL
+)
